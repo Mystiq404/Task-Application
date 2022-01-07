612181fe22612035b721983fe1fcfd31f493e88e
Mysitq404 <saurav.rwt2611@gmail.com>
2022-01-07 14:31:39 -0700

    routers

---
+++ src/routers/task.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const router = express.Router()
+const Task = require('../models/task')
+
+router.post("/tasks", async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send()
+    } catch (e) {
+        res.status(400).send(e)
+    }
+    // task.save().then(()=>{
+    //     res.status(201).send(task)
+    // }).catch((e)=>{
+    //     res.status(400).send(e)
+    // })
+})
+
+router.get('/tasks', async (req, res) => {
+
+    try {
+        const task = await Task.find({})
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+    //     Task.find({}).then((tasks)=>{
+    //         res.send(tasks)
+    //     }).catch((e)=>{
+    //         res.status(500).send()
+    //     })
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.find({ _id })
+        if (!task) {
+            res.send(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+    // Task.findById(_id).then((task)=>{
+    //     if(!task){
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((e)=>{
+    //     res.status(500).send()
+    // })
+})
+
+router.patch('/tasks/:id',async(req ,res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates= ['description' ,'completed']
+    const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
+    if(!isValidOperation){
+        return res.status(400).send({error:"Invalid Update"})
+    }
+    try{
+          const task = await Task.findByIdAndUpdate(req.params.id , req.body ,{new:true , runValidators:true})
+          if(!task){
+              res.status(404).send()
+          }
+          res.send(task)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+
+router.delete('/tasks/:id', async(req,res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            res.status(404).send({error:"No such Task found!"})
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+module.exports =router
